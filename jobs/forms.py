9d2132e6ce77bcca55b33094d13715d3b9a7f576
angusmit <angusnsh@gmail.com>
2025-06-05 00:09:46 +0100

    feat: Add user authentication with role-based signup (job seeker/employer)

--- jobs/forms.py
+++ jobs/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 from .models import Job
 
 class JobForm(forms.ModelForm):
@@ -8,3 +10,30 @@ class JobForm(forms.ModelForm):
         widgets = {
             'description': forms.Textarea(attrs={'rows': 5}),
         }
+
+class JobSeekerSignUpForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'phone')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.user_type = 'jobseeker'
+        if commit:
+            user.save()
+        return user
+
+class EmployerSignUpForm(UserCreationForm):
+    company = forms.CharField(max_length=100)
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password1', 'password2', 'phone', 'company')
+    
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.user_type = 'employer'
+        user.company = self.cleaned_data['company']
+        if commit:
+            user.save()
+        return user
